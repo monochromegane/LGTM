c877011139740c65fdcdf1247860af4bd026a0b5
shinriyo <shinriyo@gmail.com>
2017-01-28 13:48:02 +0900

    `\n` is contain message only
    `\n` is contain message only

--- content/github.js
+++ content/github.js
@@ -1,5 +1,11 @@
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    var oldMessage = $("textarea[name='comment[body]']").val();
-    $("textarea[name='comment[body]']").val(oldMessage + "\n\n" + request.image);
-});
+    function(request, sender, sendResponse) {
+        var oldMessage = $("textarea[name='comment[body]']").val();
+        var attr = $("textarea[name='comment[body]']");
+        var brStr = "";
+        if (oldMessage) {
+            brStr = "\n\n";
+        }
+        attr.val(oldMessage + brStr + request.image);
+    }
+);
